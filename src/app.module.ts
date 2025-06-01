939c681d1f2b3f7d71ed280c367e697cedd66061
daniu006 <dpabad@sudamericano.edu.ec>
2025-06-01 18:48:59 -0500

    app.module.modificado2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { NotificationsModule } from './notifications/notifications.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-    imports:[
+    imports:[ ConfigModule.forRoot({
+        isGlobal: true }),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            url: 'postgres://postgres:12345@localhost:5432/notification_db',
+            url: process.env.DATABASE_URL,
             port: 5432,
             username: 'postgres', 
             password: 'pYnsrbxCQfVpQmXzbbIzKrqGOiYyfruc',    
